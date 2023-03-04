62a8cfc34488389a5bd3e2ab370c33114c07cbe1
Kciraf <valsinat44a@mail.ru>
2023-03-04 16:49:22 +0600

    Initialisation and exercise 41

---
+++ Exercise41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] InputArray(string text)
+{
+    System.Console.WriteLine(text);
+    return  Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+
+int CountPositive(int[] arr)
+{
+    int result = -1;            // "-" На случай ошибки счета
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) result++;
+    }
+    return result++;        // добавляем 1 для компенсации приобъявлении
+}
+
+int[] numbers = InputArray("Введите числа целые числа через пробел: ");
+
+System.Console.WriteLine("Введено положительных чисел: " + CountPositive(numbers));
